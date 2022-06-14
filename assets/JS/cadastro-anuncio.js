febde323af4e873842e968fcfbba9a4878a498dd
Raniel Araujo <103837415+Raniel-AS@users.noreply.github.com>
2022-06-14 09:56:11 -0300

    Deletando Arquivos Iniciais

--- assets/JS/cadastro-anuncio.js
+++
@@ -1,9 +0,0 @@
-let uploadedImage = "";
-document.querySelector("#input-img-anuncio").addEventListener("change", function () {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        uploadedImage = reader.result;
-        document.querySelector(".img-anuncio").style.backgroundImage = `url(${uploadedImage})`
-    });
-    reader.readAsDataURL(this.files[0]);
-});
